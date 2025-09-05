e1e77bc9619a8ae5100f432b4d07349bf4145e01
ROHIT05S <rohit05prakash@gmail.com>
2025-09-05 15:53:11 +0530

    Famous Landmarks Detection  in Android with Jetpack Compose via creating an ML Model using Python and embedding into our app.

---
+++ app/src/main/java/com/example/ailandmarkdetection/presentation/LandmarkImageAnalyzer.kt
@@ -0,0 +1,34 @@
+package com.example.ailandmarkdetection.presentation
+
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import com.example.ailandmarkdetection.domain.Classification
+import com.example.ailandmarkdetection.domain.LandmarkClassifier
+
+class LandmarkImageAnalyzer(
+    private val classifier: LandmarkClassifier,
+    private val onResults: (List<Classification>) -> Unit
+):ImageAnalysis.Analyzer{
+
+    private var frameSkipCounter = 0
+
+    override fun analyze(image: ImageProxy) {
+
+        if(frameSkipCounter%60 == 0){
+            val rotationDegrees = image.imageInfo.rotationDegrees
+            val bitmap =  image.toBitmap().centerCrop(321,321)
+
+            val results = classifier.classify(bitmap,rotationDegrees)
+
+            onResults(results)
+        }
+
+        frameSkipCounter++
+
+
+
+        image.close()
+
+    }
+
+}
